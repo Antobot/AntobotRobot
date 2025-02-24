21b58ec9e602ac8978bbf28f4f647825a3749da3
Daniel Freer <daniel.freer@antobot.ai>
2025-02-24 17:31:20 +0000

    Build and run dummy ant_control node - needs correct types, and everything else

--- antobot_control/src/ant_control.cpp
+++ antobot_control/src/ant_control.cpp
@@ -1,169 +1,63 @@
-// Copyright 2020 ros2_control Development Team
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include "antobot_control/ant_control.hpp"
-
 #include <chrono>
-#include <cmath>
-#include <iomanip>
-#include <limits>
+#include <functional>
 #include <memory>
-#include <sstream>
 #include <string>
-#include <vector>
 
-#include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
-namespace ros2_control_demo_example_1
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+
+
+class AntobotControl : public rclcpp::Node
 {
-hardware_interface::CallbackReturn RRBotSystemPositionOnlyHardware::on_init(
-  const hardware_interface::HardwareInfo & info)
-{
-  if (
-    hardware_interface::SystemInterface::on_init(info) !=
-    hardware_interface::CallbackReturn::SUCCESS)
-  {
-    return hardware_interface::CallbackReturn::ERROR;
-  }
-  logger_ = std::make_shared<rclcpp::Logger>(
-    rclcpp::get_logger("controller_manager.resource_manager.hardware_component.system.RRBot"));
-  clock_ = std::make_shared<rclcpp::Clock>(rclcpp::Clock());
-
-  hw_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
-  hw_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
-
-  for (const hardware_interface::ComponentInfo & joint : info_.joints)
-  {
-    // RRBotSystemPositionOnly has exactly one state and command interface on each joint
-    if (joint.command_interfaces.size() != 1)
+  public:
+    AntobotControl() : Node("antobot_control"), count_(0)
     {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' has %zu command interfaces found. 1 expected.",
-        joint.name.c_str(), joint.command_interfaces.size());
-      return hardware_interface::CallbackReturn::ERROR;
+      
+        sub_wheel_vel_ = this->create_subscription<std_msgs::msg::String>("/antobot/bridge/wheel_vel", 10, 
+            std::bind(&AntobotControl::wheel_vel_callback, this, _1));
+        sub_robot_cmd_vel_ = this->create_subscription<std_msgs::msg::String>("/antobot/robot/cmd_vel", 10,
+            std::bind(&AntobotControl::robot_cmd_vel_callback, this, _1));
+
+
+        pub_wheel_vel_cmd_ = this->create_publisher<std_msgs::msg::String>("/antobot/control/wheel_vel_cmd", 10);
+        timer_ = this->create_wall_timer(500ms, std::bind(&AntobotControl::timer_callback, this));
     }
 
-    if (joint.command_interfaces[0].name != hardware_interface::HW_IF_POSITION)
+  private:
+    void timer_callback()
     {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' have %s command interfaces found. '%s' expected.",
-        joint.name.c_str(), joint.command_interfaces[0].name.c_str(),
-        hardware_interface::HW_IF_POSITION);
-      return hardware_interface::CallbackReturn::ERROR;
+        auto message = std_msgs::msg::String();
+        message.data = "Hello, world! " + std::to_string(count_++);
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        pub_wheel_vel_cmd_->publish(message);
     }
 
-    if (joint.state_interfaces.size() != 1)
+    void wheel_vel_callback(const std_msgs::msg::String &msg) const
     {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' has %zu state interface. 1 expected.", joint.name.c_str(),
-        joint.state_interfaces.size());
-      return hardware_interface::CallbackReturn::ERROR;
+        RCLCPP_INFO(this->get_logger(), "in wheel vel callback");
     }
 
-    if (joint.state_interfaces[0].name != hardware_interface::HW_IF_POSITION)
+    void robot_cmd_vel_callback(const std_msgs::msg::String &msg) const
     {
-      RCLCPP_FATAL(
-        get_logger(), "Joint '%s' have %s state interface. '%s' expected.", joint.name.c_str(),
-        joint.state_interfaces[0].name.c_str(), hardware_interface::HW_IF_POSITION);
-      return hardware_interface::CallbackReturn::ERROR;
+        RCLCPP_INFO(this->get_logger(), "in robot cmd vel callback");
     }
-  }
 
-  return hardware_interface::CallbackReturn::SUCCESS;
-}
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_wheel_vel_cmd_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_wheel_vel_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_robot_cmd_vel_;
 
-hardware_interface::CallbackReturn RRBotSystemPositionOnlyHardware::on_configure(
-  const rclcpp_lifecycle::State & /*previous_state*/)
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
 {
-
-  // reset values always when configuring hardware
-  for (uint i = 0; i < hw_states_.size(); i++)
-  {
-    hw_states_[i] = 0;
-    hw_commands_[i] = 0;
-  }
-  RCLCPP_INFO(get_logger(), "Successfully configured!");
-
-  return hardware_interface::CallbackReturn::SUCCESS;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<AntobotControl>());
+    rclcpp::shutdown();
+    return 0;
 }
-
-std::vector<hardware_interface::StateInterface>
-RRBotSystemPositionOnlyHardware::export_state_interfaces()
-{
-  std::vector<hardware_interface::StateInterface> state_interfaces;
-  for (uint i = 0; i < info_.joints.size(); i++)
-  {
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_states_[i]));
-  }
-
-  return state_interfaces;
-}
-
-std::vector<hardware_interface::CommandInterface>
-RRBotSystemPositionOnlyHardware::export_command_interfaces()
-{
-  std::vector<hardware_interface::CommandInterface> command_interfaces;
-  for (uint i = 0; i < info_.joints.size(); i++)
-  {
-    command_interfaces.emplace_back(hardware_interface::CommandInterface(
-      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_commands_[i]));
-  }
-
-  return command_interfaces;
-}
-
-hardware_interface::CallbackReturn RRBotSystemPositionOnlyHardware::on_activate(
-  const rclcpp_lifecycle::State & /*previous_state*/)
-{
-  // command and state should be equal when starting
-  for (uint i = 0; i < hw_states_.size(); i++)
-  {
-    hw_commands_[i] = hw_states_[i];
-  }
-
-  RCLCPP_INFO(get_logger(), "Successfully activated!");
-
-  return hardware_interface::CallbackReturn::SUCCESS;
-}
-
-hardware_interface::CallbackReturn RRBotSystemPositionOnlyHardware::on_deactivate(
-  const rclcpp_lifecycle::State & /*previous_state*/)
-{
-  return hardware_interface::CallbackReturn::SUCCESS;
-}
-
-hardware_interface::return_type RRBotSystemPositionOnlyHardware::read(
-  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
-{
-  return hardware_interface::return_type::OK;
-}
-
-hardware_interface::return_type RRBotSystemPositionOnlyHardware::write(
-  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
-{
-  return hardware_interface::return_type::OK;
-}
-
-}  // namespace ros2_control_demo_example_1
-
-int main()
-{}
-
-#include "pluginlib/class_list_macros.hpp"
-
-PLUGINLIB_EXPORT_CLASS(
-  ros2_control_demo_example_1::RRBotSystemPositionOnlyHardware, hardware_interface::SystemInterface)
-
