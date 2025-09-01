8b03a4aa441d7af968bbe2b420eb12d80a0f09ee
So Young Kim <soyoung.kim@antobot.ai>
2025-09-01 15:13:08 +0800

    Add costmap footprint, lifecycle manager in launch file to handle configuration/activation automatically

--- antobot_nav2_costmap/launch/costmap_launch.py
+++ antobot_nav2_costmap/launch/costmap_launch.py
@@ -1,7 +1,9 @@
+import os
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-import os
+
 
 def generate_launch_description():
     # Path to your costmap YAML
@@ -11,13 +13,29 @@ def generate_launch_description():
         'local_costmap.yaml'
     )
 
-    return LaunchDescription([
-        Node(
-            package='nav2_costmap_2d_test',
-            executable='nav2_costmap_2d_test',
-            name='costmap',        # ROS 2 node name
-            output='screen',
-            parameters=[config_file]
-        )
+    # Costmap node
+    costmap_node = Node(
+        package='nav2_costmap_2d',
+        executable='nav2_costmap_2d',
+        name='costmap',
+        output='screen',
+        parameters=[config_file]
+    )
 
+    # Lifecycle manager
+    lifecycle_manager = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_costmap',
+        output='screen',
+        parameters=[{
+            'autostart': True,          # bring nodes up automatically
+            'node_names': ['costmap/costmap'],  # list of managed lifecycle nodes
+            'bond_timeout': 0.0,       
+        }]
+    )
+
+    return LaunchDescription([
+        costmap_node,
+        lifecycle_manager
     ])
