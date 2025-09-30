c8a6f07300948b91e0b29f136045d1d08662e35c
huaide <huaide.wang@nicecart.ai>
2025-09-30 09:54:15 +0100

    feat: launch different type joystick

--- antobot_teleop/launch/teleop_launch.py
+++ antobot_teleop/launch/teleop_launch.py
@@ -4,7 +4,7 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, LogInfo
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
@@ -17,26 +17,36 @@ def generate_launch_description():
         'use_keyboard', default_value='true', description='Use Keyboard input'
     )
 
+    # TODO
+    joystick = DeclareLaunchArgument(
+        'joystick', default_value='POCKETT', description='Joystick Type'
+    )
+
     # Get the path to the YAML configuration file
     joy_remap_config = os.path.join(
         get_package_share_directory('antobot_teleop'), 
         'config',
         'gamepad.yaml'  
     )
-    print(joy_remap_config)
 
     # Define nodes
     joy_node = Node(
-        condition=IfCondition(LaunchConfiguration('use_joy')),
+        condition=IfCondition(PythonExpression([
+            "'", LaunchConfiguration('use_joy'), "' == 'true' and '", 
+            LaunchConfiguration('joystick'), "' == 'LogitechF710'"
+        ])),
         package='joy',
         executable='joy_node',
         name='joy_node',
         parameters=[{'dev': '/dev/input/js0'}],
         remappings=[('joy', 'joy_orig')],
     )
-
+	
     joy_remap_node = Node(
-        condition=IfCondition(LaunchConfiguration('use_joy')),
+        condition=IfCondition(PythonExpression([
+            "'", LaunchConfiguration('use_joy'), "' == 'true' and '", 
+            LaunchConfiguration('joystick'), "' == 'LogitechF710'"
+        ])),
         package='antobot_teleop',
         executable='joy_remap',
         name='joy_remap',
@@ -45,6 +55,16 @@ def generate_launch_description():
 
     )
 
+    joy_elrs_node_node = Node(
+        condition=IfCondition(PythonExpression([
+            "'", LaunchConfiguration('use_joy'), "' == 'true' and '", 
+            LaunchConfiguration('joystick'), "' == 'POCKETT'"
+        ])),
+        package='antobot_devices_joy',
+        executable='joy_elrs_node',
+        name='joy_elrs_node'
+    )
+    
     teleop_node = Node(
         condition=IfCondition(LaunchConfiguration('use_keyboard')),
         package='antobot_teleop',
@@ -55,8 +75,9 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        use_joy,use_keyboard,       # Declare the launch argument
+        use_joy,use_keyboard,joystick,       # Declare the launch argument
         joy_node,       # Start joy node if use_joy is true
         joy_remap_node, # Start joy remap node if use_joy is true
+        joy_elrs_node_node,
         teleop_node,    # Start the teleop node
     ])
